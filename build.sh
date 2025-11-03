b10cb576d2f62215adae569bb5b6e1bbe6a83a21
LUAN LIMA FERREIRA <ferreira.luan@alunos.senac.rio>
2025-11-03 20:55:26 -0300

    FIX: Reverte para os caminhos corretos do manage.py no build e start

--- build.sh
+++ build.sh
@@ -5,14 +5,12 @@ set -o errexit
 # 1. Instala as dependências
 pip install -r requirements.txt
 
-# 2. Roda o collectstatic
-# (Apontando para a pasta correta do manage.py)
-python projetorestaurante/projetorestaurante/manage.py collectstatic --no-input
+# 2. Roda o collectstatic (Caminho correto)
+python projetorestaurante/manage.py collectstatic --no-input
 
-# 3. Roda as migrações
-# (Apontando para a pasta correta do manage.py)
-python projetorestaurante/projetorestaurante/manage.py migrate --no-input
+# 3. Roda as migrações (Caminho correto)
+python projetorestaurante/manage.py migrate --no-input
 
-# 4. Carrega os dados
-# (O loaddata usa o manage.py como referência para encontrar o initial_data.json)
-python projetorestaurante/projetorestaurante/manage.py loaddata initial_data.json
+# 4. Carrega os dados (Caminho correto)
+# O manage.py está em 'projetorestaurante/', e o .json também, então ele o encontrará.
+python projetorestaurante/manage.py loaddata initial_data.json
