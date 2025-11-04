f799793a4e9d524404e5728deb9c65de39f6404f
LUAN LIMA FERREIRA <ferreira.luan@aluno.senac.rio>
2025-11-03 21:25:40 -0300

    FIX: Fornece o caminho completo para o initial_data.json no loaddata

--- build.sh
+++ build.sh
@@ -5,11 +5,11 @@ set -o errexit
 # 1. Instala as dependências
 pip install -r requirements.txt
 
-# 2. Roda o collectstatic (usando o caminho correto)
+# 2. Roda o collectstatic (Caminho correto)
 python projetorestaurante/projetorestaurante/manage.py collectstatic --no-input
 
-# 3. Roda as migrações (usando o caminho correto)
+# 3. Roda as migrações (Caminho correto)
 python projetorestaurante/projetorestaurante/manage.py migrate --no-input
 
-# 4. Carrega os dados (o initial_data.json deve estar na mesma pasta do manage.py)
-python projetorestaurante/projetorestaurante/manage.py loaddata initial_data.json
+# 4. Carrega os dados (CORREÇÃO AQUI: Fornecendo o caminho completo)
+python projetorestaurante/projetorestaurante/manage.py loaddata projetorestaurante/projetorestaurante/initial_data.json
