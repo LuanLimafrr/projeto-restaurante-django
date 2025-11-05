36719e1ce6d2790764f925a2f4ff6f78df78b7ef
LUAN LIMA FERREIRA <ferreira.luan@alunos.senac.rio>
2025-11-05 18:26:23 -0300

    FIX: Corrige caminhos do manage.py para a estrutura aninhada correta

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -o errexit
 # 1. Instala as dependências
 pip install -r requirements.txt
 
-# 2. Roda o collectstatic (Caminho correto)
-python projetorestaurante/manage.py collectstatic --no-input
+# 2. Roda o collectstatic (usando o caminho correto)
+python projetorestaurante/projetorestaurante/manage.py collectstatic --no-input
 
-# 3. Roda as migrações (Caminho correto)
-python projetorestaurante/manage.py migrate --no-input
+# 3. Roda as migrações (usando o caminho correto)
+python projetorestaurante/projetorestaurante/manage.py migrate --no-input
 
-# 4. LIMPA o banco de dados de dados antigos (Resolve o erro "duplicate key")
-python projetorestaurante/manage.py flush --no-input
+# 4. LIMPA o banco de dados de dados antigos
+python projetorestaurante/projetorestaurante/manage.py flush --no-input
 
-# 5. Carrega os dados (O manage.py e o initial_data.json estão na mesma pasta)
-python projetorestaurante/manage.py loaddata projetorestaurante/initial_data.json
+# 5. Carrega os dados (Assumindo que o JSON está ao lado do manage.py)
+python projetorestaurante/projetorestaurante/manage.py loaddata projetorestaurante/projetorestaurante/initial_data.json
