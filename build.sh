059105a667984c87103715a2aaf4a7bcd4c7ec7b
LUAN LIMA FERREIRA <ferreira.luan@alunos.senac.rio>
2025-11-05 18:22:35 -0300

    FIX: Corrige caminhos do manage.py para a estrutura de pastas correta

--- build.sh
+++ build.sh
@@ -6,10 +6,13 @@ set -o errexit
 pip install -r requirements.txt
 
 # 2. Roda o collectstatic (Caminho correto)
-python projetorestaurante/projetorestaurante/manage.py collectstatic --no-input
+python projetorestaurante/manage.py collectstatic --no-input
 
 # 3. Roda as migrações (Caminho correto)
-python projetorestaurante/projetorestaurante/manage.py migrate --no-input
+python projetorestaurante/manage.py migrate --no-input
 
-# 4. Carrega os dados (CORREÇÃO AQUI: Fornecendo o caminho completo)
-python projetorestaurante/projetorestaurante/manage.py loaddata projetorestaurante/projetorestaurante/initial_data.json
+# 4. LIMPA o banco de dados de dados antigos (Resolve o erro "duplicate key")
+python projetorestaurante/manage.py flush --no-input
+
+# 5. Carrega os dados (O manage.py e o initial_data.json estão na mesma pasta)
+python projetorestaurante/manage.py loaddata projetorestaurante/initial_data.json
