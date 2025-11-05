f943f600dcfd7c982eceda740b2517e16bbe38b2
LUAN LIMA FERREIRA <ferreira.luan@alunos.senac.rio>
2025-11-05 18:53:10 -0300

    FIX: Adiciona comando createsuperuser ao build

--- build.sh
+++ build.sh
@@ -5,14 +5,18 @@ set -o errexit
 # 1. Instala as dependências
 pip install -r requirements.txt
 
-# 2. Roda o collectstatic (usando o caminho correto)
+# 2. Roda o collectstatic
 python projetorestaurante/projetorestaurante/manage.py collectstatic --no-input
 
-# 3. Roda as migrações (usando o caminho correto)
+# 3. Roda as migrações (Cria as tabelas)
 python projetorestaurante/projetorestaurante/manage.py migrate --no-input
 
-# 4. LIMPA o banco de dados de dados antigos
+# 4. LIMPA o banco de dados
 python projetorestaurante/projetorestaurante/manage.py flush --no-input
 
-# 5. Carrega os dados (Assumindo que o JSON está ao lado do manage.py)
+# 5. Carrega os dados do Cardápio
 python projetorestaurante/projetorestaurante/manage.py loaddata projetorestaurante/projetorestaurante/initial_data.json
+
+# 6. CRIA O SUPERUSUÁRIO (ESTE É O COMANDO QUE FALTAVA)
+# (Usa as variáveis que você configurou no Render)
+python projetorestaurante/projetorestaurante/manage.py createsuperuser --noinput
