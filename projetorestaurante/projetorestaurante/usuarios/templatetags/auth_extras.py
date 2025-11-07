0bf5fd27ce5050132cc92fd6d448e4e5c48747df
LUAN LIMA FERREIRA <ferreira.luan@aluno.senac.rio>
2025-11-07 19:09:12 -0300

    FIX: Corrige erro 500 no dashboard_staff.html (substitui is_gerente por filtro has_group)

--- projetorestaurante/projetorestaurante/usuarios/templatetags/auth_extras.py
+++ projetorestaurante/projetorestaurante/usuarios/templatetags/auth_extras.py
@@ -5,13 +5,8 @@ register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    """
-    Verifica se um usuário pertence a um grupo específico.
-    Uso no template: {% if request.user|has_group:"Gerente" %}
-    """
     try:
         group = Group.objects.get(name=group_name)
     except Group.DoesNotExist:
         return False
-
-    return group in user.groups.all()
+    return group in user.groups.all()   
